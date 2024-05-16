4331157a24f512ed050d5334db658700ea813ff2
William S. Tanil <tanilwilliam@gmail.com>
2024-05-16 16:00:48 +0700

    Initial Commit

---
+++ Motivation/MotivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationApp.swift
+//  Motivation
+//
+//  Created by William Suryadi Tanil on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MotivationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
