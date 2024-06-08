5be5a3c9821c1a51acfeff5f0c50b70c2d853646
William S. Tanil <william_strik3_tanil2@yahoo.co.id>
2024-06-08 15:39:52 +0700

    Add placeholder for Setting

--- Motivation/SettingView.swift
+++ Motivation/SettingView.swift
@@ -9,17 +9,64 @@
 import SwiftUI
 
 struct SettingView: View {
+    
+    @AppStorage("showCustomGoal") private var showCustomGoal = false
+    @AppStorage("isGoalPublic") private var isGoalPublic = false
+    @AppStorage("customGoal") private var customGoal = ""
+    
     var body: some View {
         
-        VStack {
-            Text("Hi, what do you want to change today?")
-            Text("Do you want to write your goal?")
-            Text("-Write your goal here")
-            Text("-Do you want to show your goal by default?")
-            Text("How do you want to be reminded?")
-            
-            
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
+                    HStack() {
+                        Text("Hello, what do you want to change today?")
+                            .font(.title)
+                    }
+                    
+                    Spacer(minLength: 8)
+                    
+                    HStack {
+                        VStack(alignment: .leading) {
+                            
+                            VStack(alignment: .leading) {
+                                Text("Do you want to write your goal?")
+                                HStack {
+                                    Button { self.showCustomGoal = true } label: {
+                                        Text("Yes")
+                                    }
+                                    Button { self.showCustomGoal = false } label: {
+                                        Text("No")
+                                    }
+                                }
+                                if showCustomGoal {
+                                    TextField("Write your goal here", text: $customGoal)
+                                    Text("-Do you want to show your goal by default?")
+                                    HStack {
+                                        Button { self.isGoalPublic = true } label: {
+                                            Text("Yes")
+                                        }
+                                        Button { self.isGoalPublic = false } label: {
+                                            Text("No")
+                                        }
+                                    }
+                                }
+                            }
+                            
+                            
+                            
+                            
+                            Text("How do you want to be reminded?")
+                        }
+                        Spacer()
+                    }
+                    
+                }
+                .padding()
+                
+            }
         }
+        
         
     }
 }
